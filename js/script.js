f09b913f7bc82b35e5b1e70f4610ca68fb56e1c0
amiredwardalpha <alpha.engineering.sec@gmail.com>
2020-07-16 11:07:32 +0300

    Alpha

---
+++ js/script.js
@@ -0,0 +1,13 @@
+function hoverbtn (){
+    document.getElementById("btn1").style.backgroundColor = "deepskyblue";
+}
+function leavebtn (){
+    document.getElementById("btn1").style.backgroundColor = "#00BCBA";
+}
+function show()
+{
+    document.getElementById("di").style.visibility = "visible" ;
+ }
+function hide (){
+    document.getElementById("di").style.visibility = "hidden";
+}
